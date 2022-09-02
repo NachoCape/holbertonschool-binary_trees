95cd944e4062b9e8493e3ee693048c3775bfdc02
NachoCape <igcapezzolo@hotmail.com>
2022-09-01 21:52:29 -0700

    fix NULL border case

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,8 +12,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t h_left, h_right;
 
 	if (!tree)
-		return (-1);
-	h_left = 1 + binary_tree_height(tree->left);
-	h_right = 1 + binary_tree_height(tree->right);
+		return (0);
+	h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (h_left > h_right ? h_left : h_right);
 }
