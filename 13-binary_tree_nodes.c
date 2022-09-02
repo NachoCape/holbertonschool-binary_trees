a8ab0a9d166fe49ce8134a8787655b0166d20156
RicardoDanta <uruguaydenos@gmail.com>
2022-09-02 12:18:05 -0700

    Change the 13th

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - function that counts the nodes with at least 1 child in a binary tree
+ * binary_tree_nodes - function that counts the nodes
+ * with at least 1 child in a binary tree
  * @tree: pointer to the root node of the tree to count the number of nodes
  * Return: If tree is NULL, the function must return 0
  */
@@ -17,7 +18,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	left_nodes = binary_tree_nodes(tree->left);
+	left_nodes = binary_tree_nodes(tree->left) + 1;
 	right_nodes = binary_tree_nodes(tree->right);
 
 	return (left_nodes + right_nodes);
